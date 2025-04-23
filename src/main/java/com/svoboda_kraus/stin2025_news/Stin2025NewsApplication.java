ab0ca663883f3dd5855cbc706b7fa2149aa34a40
Jan Svoboda <jan.svoboda1@tul.cz>
2025-04-23 23:26:20 +0200

    Initial commit

---
+++ src/main/java/com/svoboda_kraus/stin2025_news/Stin2025NewsApplication.java
@@ -0,0 +1,13 @@
+package com.svoboda_kraus.stin2025_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stin2025NewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stin2025NewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/svoboda_kraus/stin2025_news/Stin2025NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svoboda_kraus.stin2025_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stin2025NewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
